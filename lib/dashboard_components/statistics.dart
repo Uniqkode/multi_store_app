92d67624b9a8ebf014f903f65ae8ca59ccc6a372
uniqkode <yungboox@email.com>
2023-03-31 08:48:15 +0100

    add balance screen

--- lib/dashboard_components/statistics.dart
+++ lib/dashboard_components/statistics.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/appBar_widgets.dart';
@@ -7,14 +9,163 @@ class Statistics extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        leading: const AppBarBackButton(),
-        backgroundColor: Colors.white,
-        title: const AppBarTitle(title: 'Statistics'),
-      ),
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('orders')
+            .where('sid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Material(
+              child: Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.purpleAccent,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    Colors.purple,
+                  ),
+                ),
+              ),
+            );
+          }
+
+          num itemCount = 0;
+          for (var item in snapshot.data!.docs) {
+            itemCount += item['orderqty'];
+          }
+
+          double totalPrice = 0.0;
+          for (var item in snapshot.data!.docs) {
+            totalPrice += item['orderqty'] * item['orderprice'];
+          }
+
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              elevation: 0,
+              leading: const AppBarBackButton(),
+              backgroundColor: Colors.white,
+              title: const AppBarTitle(title: 'Statistics'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  StatisticsModel(
+                    label: 'sold out',
+                    value: snapshot.data!.docs.length,
+                    decimal: 0,
+                  ),
+                  StatisticsModel(
+                    label: 'item count',
+                    value: itemCount,
+                    decimal: 0,
+                  ),
+                  StatisticsModel(
+                    label: 'total balance',
+                    value: totalPrice,
+                    decimal: 2,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
+
+class StatisticsModel extends StatelessWidget {
+  final String label;
+  final dynamic value;
+  final int decimal;
+  const StatisticsModel({
+    Key? key,
+    required this.label,
+    required this.value,
+    required this.decimal,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 60,
+          width: MediaQuery.of(context).size.width * .55,
+          decoration: const BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25), topRight: Radius.circular(25))),
+          child: Center(
+              child: Text(
+            label.toUpperCase(),
+            style: const TextStyle(fontSize: 20, color: Colors.white),
+          )),
+        ),
+        Container(
+          height: 90,
+          width: MediaQuery.of(context).size.width * .7,
+          decoration: BoxDecoration(
+              color: Colors.blueGrey.shade100,
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25))),
+          child: AnimatedCounter(
+            count: value,
+            decimal: decimal,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AnimatedCounter extends StatefulWidget {
+  final dynamic count;
+  final int decimal;
+
+  const AnimatedCounter({Key? key, required this.count, required this.decimal})
+      : super(key: key);
+
+  @override
+  State<AnimatedCounter> createState() => _AnimatedCounterState();
+}
+
+class _AnimatedCounterState extends State<AnimatedCounter>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _animation;
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _animation = _controller;
+    setState(() {
+      _animation = Tween(begin: _animation.value, end: widget.count)
+          .animate(_controller);
+    });
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Center(
+            child: Text(
+          _animation.value.toStringAsFixed(widget.decimal),
+          style: const TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Acme',
+              letterSpacing: 2,
+              color: Colors.pink),
+        ));
+      },
     );
   }
 }
